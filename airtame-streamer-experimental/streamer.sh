e7d67983a1b50119b40ea93311823cc5e292c26c
Lars Hagström <lars@foldspace.nu>
2015-01-19 08:59:47 +0100

    move the experimental stuff to own pkgbuild

--- airtame-streamer-experimental/streamer.sh
+++ airtame-streamer-experimental/streamer.sh
@@ -4,6 +4,6 @@ mkdir -p $HOME/.config/airtame/logs
 cd $HOME/.config/airtame
 export LD_LIBRARY_PATH=/opt/airtame/lib
 export AIRTAME_LOG_PATH=$HOME/.config/airtame/logs
-export AIRTAME_MODULES=/opt/airtame/lib/airtame-modules
+export AIRTAME_MODULES=/opt/airtame/modules
 
 /opt/airtame/bin/airtame-server $@
